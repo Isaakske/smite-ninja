59a4d6520aa22016aef1a45221fda2c9de256bfd
Isaakske <isaak.de.backer@hotmail.com>
2022-02-07 20:06:47 +0100

    remember playername for 14 days

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -10,17 +10,18 @@ use App\Form\Data\PlayerSearchData;
 use App\Form\Type\PlayerSearchType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlayerController extends AbstractController
 {
     #[Route('/', name: 'player_search', methods: ['GET', 'POST'])]
     #[Template]
-    public function search(Request $request, Smite $smite): array|RedirectResponse
+    public function search(Request $request, Smite $smite): Response
     {
-        $data = new PlayerSearchData();
+        $data = new PlayerSearchData($request->cookies->get('player_name'));
 
         $form = $this->createForm(PlayerSearchType::class, $data);
 
@@ -29,6 +30,8 @@ class PlayerController extends AbstractController
 
             if ($form->isSubmitted() && $form->isValid()) {
                 if ($playerName = $data->getPlayerName()) {
+                    $cookie = Cookie::create('player_name', $playerName, new \DateTime('+14 days'));
+
                     $playerData = $smite->searchPlayers($playerName);
 
                     $matchingPlayers = array_filter($playerData, static fn (array $data) => $data['Name'] === $playerName);
@@ -36,7 +39,10 @@ class PlayerController extends AbstractController
                     if (count($matchingPlayers) === 1) {
                         $player = reset($matchingPlayers);
 
-                        return $this->redirectToRoute('match_live', ['playerId' => $player['player_id']]);
+                        $response = $this->redirectToRoute('match_live', ['playerId' => $player['player_id']]);
+                        $response->headers->setCookie($cookie);
+
+                        return $response;
                     }
 
                     $players = array_map(static fn (array $data) => Player::createFromData($data), $playerData);
@@ -44,9 +50,15 @@ class PlayerController extends AbstractController
             }
         }
 
-        return [
+        $response = $this->render('player/search.html.twig', [
             'form' => $form->createView(),
             'players' => $players ?? [],
-        ];
+        ]);
+
+        if (isset($cookie)) {
+            $response->headers->setCookie($cookie);
+        }
+
+        return $response;
     }
 }
