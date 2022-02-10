76000c4816731312d05e9cb928f881903860e14b
Isaakske <isaak.de.backer@hotmail.com>
2022-02-10 13:32:58 +0100

    display rank in match

--- src/Entity/AccountInfo.php
+++ src/Entity/AccountInfo.php
@@ -7,15 +7,42 @@ namespace App\Entity;
 class AccountInfo
 {
     private int $id;
+    private string $name;
+    private int $level;
+    private int $rank;
+    private int $mmr;
+    private int $wins;
+    private int $losses;
+    private int $ratio;
 
-    public function __construct(int $id)
+    public function __construct(int $id, string $name, int $level, int $rank, int $mmr, int $wins, int $losses, int $ratio)
     {
         $this->id = $id;
+        $this->name = $name;
+        $this->level = $level;
+        $this->rank = $rank;
+        $this->mmr = $mmr;
+        $this->wins = $wins;
+        $this->losses = $losses;
+        $this->ratio = $ratio;
     }
 
     public static function createFromData(array $data): self
     {
-        return new self((int) $data['Id']);
+        $wins = (int) $data['RankedConquest']['Wins'];
+        $losses = (int) $data['RankedConquest']['Losses'];
+        $total = $wins + $losses;
+
+        return new self(
+            (int) $data['Id'],
+            $data['Name'],
+            (int) $data['Level'],
+            (int) $data['RankedConquest']['Tier'],
+            (int) $data['RankedConquest']['Rank_Stat'],
+            $wins,
+            $losses,
+            $total ? (int) round(($wins / $total) * 100) : 0
+        );
     }
 
     public function getId(): int
@@ -26,5 +53,79 @@ class AccountInfo
     public function setId(int $id): void
     {
         $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        if (str_contains($name, ']')) {
+            $name = str_replace(']', '] ', $name);
+        }
+
+        $this->name = $name;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): void
+    {
+        $this->level = $level;
+    }
+
+    public function getRank(): int
+    {
+        return $this->rank;
+    }
+
+    public function setRank(int $rank): void
+    {
+        $this->rank = $rank;
+    }
+
+    public function getMmr(): int
+    {
+        return $this->mmr;
+    }
+
+    public function setMmr(int $mmr): void
+    {
+        $this->mmr = $mmr;
+    }
+
+    public function getWins(): int
+    {
+        return $this->wins;
+    }
+
+    public function setWins(int $wins): void
+    {
+        $this->wins = $wins;
+    }
+
+    public function getLosses(): int
+    {
+        return $this->losses;
+    }
+
+    public function setLosses(int $losses): void
+    {
+        $this->losses = $losses;
+    }
+
+    public function getRatio(): int
+    {
+        return $this->ratio;
+    }
+
+    public function setRatio(int $ratio): void
+    {
+        $this->ratio = $ratio;
     }
 }
--- src/Twig/Extension/NameExtension.php
+++ src/Twig/Extension/NameExtension.php
@@ -14,6 +14,7 @@ class NameExtension extends AbstractExtension
         return [
             new TwigFunction('portal_name', [$this, 'getPortalName']),
             new TwigFunction('mode_name', [$this, 'getModeName']),
+            new TwigFunction('rank_name', [$this, 'getRankName']),
         ];
     }
 
@@ -34,6 +35,41 @@ class NameExtension extends AbstractExtension
     public function getModeName(int $mode): string
     {
         switch ($mode) {
+            default: return 'Unknown';
+        }
+    }
+
+    public function getRankName(int $rank): string
+    {
+        switch ($rank) {
+            case 0: return 'Unranked';
+            case 1: return 'Bronze V';
+            case 2: return 'Bronze IV';
+            case 3: return 'Bronze III';
+            case 4: return 'Bronze II';
+            case 5: return 'Bronze I';
+            case 6: return 'Silver V';
+            case 7: return 'Silver IV';
+            case 8: return 'Silver III';
+            case 9: return 'Silver II';
+            case 10: return 'Silver I';
+            case 11: return 'Gold V';
+            case 12: return 'Gold IV';
+            case 13: return 'Gold III';
+            case 14: return 'Gold II';
+            case 15: return 'Gold I';
+            case 16: return 'Platinum V';
+            case 17: return 'Platinum IV';
+            case 18: return 'Platinum III';
+            case 19: return 'Platinum II';
+            case 20: return 'Platinum I';
+            case 21: return 'Diamond V';
+            case 22: return 'Diamond IV';
+            case 23: return 'Diamond III';
+            case 24: return 'Diamond II';
+            case 25: return 'Diamond I';
+            case 26: return 'Master';
+            case 27: return 'Grandmaster';
             default: return 'Unknown';
         }
     }
